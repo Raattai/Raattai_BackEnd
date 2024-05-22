1c8403d9216b6bb9d61c4334f2537110d5f9cad2
VTSRepo <vibhavatechnologyservices@gmail.com>
2024-05-23 07:05:56 +0930

    checkout finish changes

--- routes/order.js
+++ routes/order.js
@@ -2,80 +2,24 @@ var express = require("express");
 var router = express.Router();
 var Order = require("../models/order.js");
 
-router.get("/latestOrder", async function (req, res) {
+router.get("/order/:_id", async function (req, res) {
   try {
-    let orderNumber = 0;
-    // Find the user's cart
-    const orders = await Order.find();
-    if (orders.length > 0) {
-      orderNumber = orders[orders.length - 1].orderNumber + 1;
-    } else {
-      return res.status(200).json({ orderNumber: orderNumber });
+    const order_id = req.params._id;
+    if (!order_id) {
+      return res.status(401).json({ error: "Unauthorized" });
     }
-
-    return res.status(200).json({ orderNumber: orderNumber });
+    console.log(order_id);
+    const ordDetail = await Order.findOne({ order_id: order_id });
+    console.log(ordDetail);
+    if (!ordDetail) {
+      return res.status(404).json({ error: "Txn not found for the user" });
+    }
+    return res.status(200).json({ order: ordDetail });
   } catch (error) {
-    console.error("Error fetching orders:", error);
+    console.error("Error fetching Txn Details:", error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-async function latestOrder() {
-  try {
-    let orderNumber = 0;
-    // Find the user's cart
-    const orders = await Order.find();
-    if (orders.length > 0) {
-      orderNumber = orders[orders.length - 1].orderNumber + 1;
-      return orderNumber;
-    } else {
-        return orderNumber + 1;
-    }
-  } catch (error) {
-    console.error("Error fetching orders:", error);
-    return 'error';
-  }
-};
-
-async function to_store_order_transaction(res) {
-  try {
-      const order_transaction = new Order(res);
-       const result= await order_transaction.save();
-        console.log(result)
-  } catch (error) {
-    console.error("Error fetching orders:", error);
-    return 'error';
-  }
- };
-
-
-
-router.get('/order/:_id', async function(req, res) {
-    try {
-        const order_id = req.params._id; 
-        if (!order_id) {
-            return res.status(401).json({ error: 'Unauthorized' });
-        }
-        console.log(order_id)
-        const ordDetail = await Order.findOne({ order_id: order_id });
-        console.log(txnDetail)
-        if (!txnDetail) {
-            return res.status(404).json({ error: 'Txn not found for the user' });
-        }
-        return res.status(200).json({ order: ordDetail});
-
-    } catch (error) {
-        console.error('Error fetching Txn Details:', error);
-        return res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
-
-// app.get("/result_order",(req,res)=>{
-//   res.send(to_store_order_transaction(temp))
-// });
-
-// to_store_order_transaction(temp)
-
 //Exports
-module.exports = latestOrder;
+module.exports = router;
--- utils/ccavResponseHandler.js
+++ utils/ccavResponseHandler.js
@@ -3,8 +3,8 @@ var http = require("http"),
   ccav = require("./ccavutil.js"),
   crypto = require("crypto"),
   qs = require("querystring");
-var order_res =  require("../routes/order.js")
-exports.postRes = function (request, response) {
+  var orders = require('../utils/order.js');
+exports.postRes = async function (request, response) {
   var ccavEncResponse = "",
     ccavResponse = "",
     workingKey = "3B2C2300169E9C59F83232DF4304D891", //Put in the 32-Bit key shared by CCAvenues.
@@ -34,86 +34,14 @@ exports.postRes = function (request, response) {
     temp[subSplit[0]] = subSplit[1];
   }
   console.log(temp);
-  const result =  order_res.to_store_order_transaction(temp)
+  const result =  await orders.to_store_order_transaction(temp);
+  console.log(result)
   response.redirect(
-      `http://localhost:4200/shop/success-payment?id=${result._id}`
-    );
+    `http://localhost:4200/shop/success-payment?id=${result._id}`
+  );
+ 
 
-  //store the  transaction details and redirect with order id to front end. 
-		/*logic to b done
-		*/
-
-//   request.on("data", function (data) {
-//     ccavEncResponse += data;
-//     ccavPOST = qs.parse(ccavEncResponse);
-//     var encryption = ccavPOST.encResp;
-//     console.log("encrytption", encryption);
-//     ccavResponse = ccav.decrypt(encryption, keyBase64, ivBase64);
-//     console.log("response", ccavResponse);
-//   });
-
-//   request.on("end", function () {
-//     var pData = "";
-//     pData = "<table border=1 cellspacing=2 cellpadding=2><tr><td>";
-//     pData = pData + ccavResponse.replace(/=/gi, "</td><td>");
-//     pData = pData.replace(/&/gi, "</td></tr><tr><td>");
-//     pData = pData + "</td></tr></table>";
-//     htmlcode =
-//       '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>Response Handler</title></head><body><center><font size="4" color="blue"><b>Response Page</b></font><br>' +
-//       pData +
-//       "</center><br></body></html>";
-//     //response.writeHeader(200, {"Content-Type": "text/html"});
-//     //response.write(htmlcode);
-//     response.redirect(
-//       "http://localhost:4200/shop/success-payment?id=6645e094ff607d97a7c6ae71"
-//     );
-//     response.end();
-//   });
 };
 
 
 
-// {
-  // order_id: '2',
-  // tracking_id: '313011438410',
-  // bank_ref_no: 'bsde9a9ffc1c52',
-  // order_status: 'Success',
-  // failure_message: '',
-  // payment_mode: 'Credit Card',
-  // card_name: 'Visa',
-  // status_code: 'null',
-  // status_message: 'Transaction is Successful',
-  // currency: 'INR',
-  // amount: '40000.00',
-  // billing_name: 'null',
-  // billing_address: '',
-  // billing_city: '',
-  // billing_state: '',
-  // billing_zip: '',
-  // billing_country: '',
-  // billing_tel: 'null',
-  // billing_email: 'null',
-  // delivery_name: '',
-  // delivery_address: '',
-  // delivery_city: '',
-  // delivery_state: '',
-  // delivery_zip: '',
-  // delivery_country: '',
-  // delivery_tel: '',
-  // merchant_param1: '',
-  // merchant_param2: '',
-  // merchant_param3: '',
-  // merchant_param4: '',
-  // merchant_param5: '',
-  // vault: 'N',
-  // offer_type: 'null',
-  // offer_code: 'null',
-  // discount_value: '0.0',
-  // mer_amount: '40000.00',
-  // eci_value: 'null',
-  // retry: 'N',
-  // response_code: '',
-  // billing_notes: '',
-  // trans_date: '18/05/2024 14:26:40',
-  // bin_country: 'UNITED STATES'
-// }
